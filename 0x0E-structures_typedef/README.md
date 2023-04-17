e8ec8410781f4c41e92baa232b387f70ec7cd479
edugichuki <muriithigichuki854@gmail.com>
2023-04-18 00:38:04 +0300

    0. Poppy

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,10 @@
+# 0x0E. C - Structures, typedef
+
+## TASKS
+
+### 0. Poppy
+* Define a new type struct dog with the following element
+* `name`, type = `char *`
+* `age`, type = `float`
+* `owner`, type = `char *`
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+  * struct dog - new type
+  * @name: name of poppy
+  * @age: age
+  * @owner: owner
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H_ */
